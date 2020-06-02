8e0baccb9d18d8a9e689e419a237df8f96910720
Gaya Kessler <gaya@theclevernode.com>
2020-06-02 16:25:18 +0200

    only update coordinates when new route is received

--- src/components/RunMap/RunMap.tsx
+++ src/components/RunMap/RunMap.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Map,
   Polyline,
@@ -15,15 +15,22 @@ import { routeDataQuery } from '../../atoms/route';
 
 const RunMap: React.FC = () => {
   const route = useRecoilValueLoadable(routeDataQuery);
+  const [coordinates, setCoordinates] = useState<LatLong[]>([]);
 
   const defaultCenter: LatLong = [51.455820, 5.785390];
-  const coordinates = (route.state === 'hasValue' && route.contents) ? route.contents.coordinates : [];
+
+
+  useEffect(() => {
+    if (route.state === 'hasValue' && route.contents) {
+      setCoordinates(route.contents.coordinates);
+    }
+  }, [route.state, route.contents]);
 
   const center = coordinates.length > 0 ? coordinates[0] : defaultCenter;
   const bounds = coordinates.length > 0 ? coordinates : undefined;
 
   return (
-    <Map center={center} bounds={bounds} zoom={13} useFlyTo>
+    <Map center={center} bounds={bounds} zoom={14} useFlyTo>
       <TileLayer
         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"

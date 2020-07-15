f48e57e97f9d012c6bfedd02acd80eaf470e6645
Gaya Kessler <gaya@theclevernode.com>
2020-07-15 10:35:59 +0200

    parse and get distance of way

--- src/server/overpass/overpass.ts
+++ src/server/overpass/overpass.ts
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import fetch from 'node-fetch';
 import querystring from 'querystring';
 import libxml from 'libxmljs';
@@ -5,9 +6,9 @@ import libxml from 'libxmljs';
 import {
   OSMData,
   Way,
-  Node,
   Tags,
-  Nodes, Ways,
+  Node,
+  LatLng,
 } from '../types';
 
 function findTagsInElement(element: libxml.Element): Tags {
@@ -34,26 +35,25 @@ function parseOSM(buffer: Buffer): Promise<OSMData> {
       way: {},
     };
 
-    data.node = xmlDoc
+    xmlDoc
       .find('node')
-      .map((node): Node => {
+      .forEach((node): void => {
         const id = node.attr('id')?.value() || '';
         const lat = parseFloat(node.attr('lat')?.value() || '0');
         const lon = parseFloat(node.attr('lon')?.value() || '0');
         const tags = findTagsInElement(node);
 
-        return {
+        data.node[id] = {
           id,
           lat,
           lon,
           tags,
         };
-      })
-      .reduce((acc, node): Nodes => ({ ...acc, [node.id]: node }), {});
+      });
 
-    data.way = xmlDoc
+    xmlDoc
       .find('way')
-      .map((way): Way => {
+      .forEach((way): void => {
         const id = way.attr('id')?.value() || '';
         const tags = findTagsInElement(way);
 
@@ -62,13 +62,12 @@ function parseOSM(buffer: Buffer): Promise<OSMData> {
           .map((nd) => (nd as libxml.Element).attr('ref')?.value() || '')
           .filter((ref) => ref !== '');
 
-        return {
+        data.way[id] = {
           id,
           tags,
           nodeRefs,
         };
-      })
-      .reduce((acc, way): Ways => ({ ...acc, [way.id]: way }), {});
+      });
 
     resolve(data);
   });
@@ -81,27 +80,97 @@ way["bicycle"!~"no"]["bicycle"!~"use_sidepath"]["highway"]
 out;
  */
 
-function fetchOverpass(): Promise<OSMData> {
+function degToRad(deg: number): number {
+  return deg * (Math.PI / 180);
+}
+
+function getDistanceFromLatLon([lat1, lon1]: LatLng, [lat2, lon2]: LatLng): number {
+  const R = 6371; // Radius of the earth in km
+  const dLat = degToRad(lat2 - lat1);
+  const dLon = degToRad(lon2 - lon1);
+  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+    + Math.cos(degToRad(lat1)) * Math.cos(degToRad(lat2))
+    * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return R * c * 1000;
+}
+
+function getWay(data: OSMData, id: string): Way {
+  const way = data.way[id];
+
+  if (!way) {
+    throw new Error(`Way ${id} not found`);
+  }
+
+  return way;
+}
+
+function getNode(data: OSMData, id: string): Node {
+  const node = data.node[id];
+
+  if (!node) {
+    throw new Error(`Node ${id} not found`);
+  }
+
+  return node;
+}
+
+function wayDistance(data: OSMData, id: string): number {
+  const way = getWay(data, id);
+
+  return way.nodeRefs.reduce((acc, current, index, refs) => {
+    if (index === refs.length - 1) {
+      return acc;
+    }
+
+    const node1 = getNode(data, refs[index]);
+    const node2 = getNode(data, refs[index + 1]);
+
+    const dist = getDistanceFromLatLon([node1.lat, node1.lon], [node2.lat, node2.lon]);
+
+    return acc + dist;
+  }, 0);
+}
+
+function fetchOverpass(): Promise<any> {
   const center = [51.455920, 5.785393];
 
   const query = `
   way["bicycle"!~"no"]["bicycle"!~"use_sidepath"]["highway"]["access"!~"private"]
-    (around: 100, ${center.join(',')});
+    (around: 500, ${center.join(',')});
   (._;>;);
   out;
   `;
 
-  return fetch(
-    'https://overpass-api.de/api/interpreter', {
-      method: 'POST',
-      body: querystring.stringify({ data: query }),
-      headers: {
-        'content-type': 'application/x-www-form-urlencoded',
+  let q = new Promise<Buffer>((resolve, reject): void => {
+    fs.readFile(`${__dirname}/example.xml`, (err, data): void => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve(data);
+    });
+  });
+
+  if (process.env.NODE_ENV === 'production') {
+    q = fetch(
+      'https://overpass-api.de/api/interpreter', {
+        method: 'POST',
+        body: querystring.stringify({ data: query }),
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded',
+        },
       },
-    },
-  )
-    .then((response) => response.buffer())
-    .then((buffer) => parseOSM(buffer));
+    )
+      .then((response) => response.buffer());
+  }
+
+  return q.then((buffer) => parseOSM(buffer)).then((data) => {
+    const distance = wayDistance(data, '446266327');
+
+    return { distance };
+  });
 }
 
 export default fetchOverpass;

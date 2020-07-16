3aca7fd7190e21eb459b4b53bf2899ebdfd695af
Gaya Kessler <gaya@theclevernode.com>
2020-07-16 13:06:34 +0200

    fetch overpass data

--- src/components/RunMap/Overpass/OverpassTest.tsx
+++ src/components/RunMap/Overpass/OverpassTest.tsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { Marker } from 'react-leaflet';
-import { Icon } from 'leaflet';
+import { Marker, Polyline } from 'react-leaflet';
+import { Icon, LeafletMouseEvent } from 'leaflet';
+import { useTheme } from '@material-ui/core';
 
 import { OSMData } from '../../../server/types';
-import { API_URL } from '../../../constants';
 
+import { API_URL } from '../../../constants';
 import junctionUrl from './Junction.png';
 import nodeUrl from './Node.png';
 
 function fetchOverpass(): Promise<OSMData> {
   return fetch(`${API_URL}/overpass`)
-    .then((res) => res.json() as Promise<OSMData>);
+    .then((res) => res.json() as Promise<{ data: OSMData }>)
+    .then((res) => res.data);
 }
 
 const JunctionIcon = new Icon({
@@ -27,6 +29,10 @@ const NodeIcon = new Icon({
 
 const OverpassTest: React.FC = () => {
   const [osmData, setOsmData] = useState<OSMData>();
+  const theme = useTheme();
+
+  const [selectedNode, setSelectedNode] = useState<number>();
+  const [selectedSegment, setSelectedSegment] = useState<number>();
 
   useEffect(() => {
     fetchOverpass().then((data) => setOsmData(data));
@@ -36,16 +42,45 @@ const OverpassTest: React.FC = () => {
     return null;
   }
 
+  const selectedSegmentNodes = selectedSegment ? osmData.segments[selectedSegment].nodeRefs : [];
+  const selectedNodeSegments = selectedNode ? osmData.nodes[selectedNode].segmentRefs : [];
+
   return (
     <>
-      {osmData.ways[6726392].nodeRefs.map((id) => osmData?.nodes[id]).map((node) => (
+      {osmData.junctionRefs.map((id) => osmData?.nodes[id]).map((node) => (
         <Marker
           key={node.id}
-          icon={NodeIcon}
+          icon={JunctionIcon}
           title={`Node: ${node.id}`}
           position={[node.lat, node.lon]}
+          opacity={selectedNode === node.id || selectedSegmentNodes.includes(node.id) ? 1 : 0.4}
+          onclick={(e: LeafletMouseEvent): void => {
+            e.originalEvent.preventDefault();
+            setSelectedNode(node.id);
+            setSelectedSegment(undefined);
+          }}
         />
       ))}
+      {Object.values(osmData.segments).map((segment) => (
+        <Polyline
+          key={segment.id}
+          color={theme.palette.primary.main}
+          opacity={
+            selectedSegment === segment.id || selectedNodeSegments.includes(segment.id) ? 1 : 0.4
+          }
+          positions={
+            segment.nodeRefs.map((n: number) => [osmData.nodes[n].lat, osmData.nodes[n].lon])
+          }
+          title={`Segment: ${segment.id}`}
+          smoothFactor={5}
+          onclick={(e: LeafletMouseEvent): void => {
+            e.originalEvent.preventDefault();
+            setSelectedNode(undefined);
+            setSelectedSegment(segment.id);
+          }}
+        />
+      ))}
+
     </>
   );
 };
